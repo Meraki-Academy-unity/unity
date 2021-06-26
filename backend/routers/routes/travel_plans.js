f81746d281a6a7448c56cfe8168de92bcae1cb3e
deyaa1997 <deyah.mosa@gmail.com>
2021-06-26 21:54:53 +0300

    Delete travel plans by id

--- backend/routers/routes/travel_plans.js
+++ backend/routers/routes/travel_plans.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
-const {createTravelPlans , getAllTravelPlans , getTravelPlansById} = require("../controllers/travel_plans");
+const {createTravelPlans , getAllTravelPlans , getTravelPlansById , deleteTravelPlansById} = require("../controllers/travel_plans");
 const travelPlansRouter = express.Router();
 // we will use "/:id"  instead of "/" for development stage until the auth is ready to use , so we can retrieve users id from token instead of params
 travelPlansRouter.post("/:user_id", createTravelPlans);
 travelPlansRouter.get("/" , getAllTravelPlans )
 travelPlansRouter.get("/:id" , getTravelPlansById )
+travelPlansRouter.delete("/:id" , deleteTravelPlansById)
 module.exports = travelPlansRouter;
