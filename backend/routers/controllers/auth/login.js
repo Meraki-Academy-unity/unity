9a688c84c0dfb5cb978fd583037513701a038b37
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-06-26 20:50:33 +0300

    Fixed Bugs

--- backend/routers/controllers/auth/login.js
+++ backend/routers/controllers/auth/login.js
@@ -8,8 +8,8 @@ const login = (req, res) => {
   const { email, password } = req.body;
   if (email && password) {
     const query = "SELECT * FROM users WHERE email = ?;";
-    const data = [email];
-    connection.query(query, data, async function (error, found, fields) {
+    const data = [email.toLowerCase()];
+    connection.query(query, data, async function (error, found) {
       if (found.length > 0) {
         await bcrypt.compare(password, found[0].password, (err, result) => {
           if (err) {
@@ -20,22 +20,22 @@ const login = (req, res) => {
               user_id: found[0].id,
             };
             const options = {
-              expiresIn: "4h",
+              expiresIn: "200h",
             };
             const secret = process.env.SECRET;
             const token = jwt.sign(payload, secret, options);
 
             res.status(200).json({ token: token });
           } else {
-            res.status(403).json("Incorrect Username and/or Password!");
+            res.status(403).json("Incorrect Email or Password!");
           }
         });
       } else {
-        res.status(403).json("Incorrect Username and/or Password!");
+        res.status(403).json("Incorrect Email or Password!");
       }
     });
   } else {
-    res.status(403).json("Please enter Username and Password!");
+    res.status(403).json("Please enter Email and Password!");
   }
 };
 
