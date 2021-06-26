9110b2fe4d71797b0d714cc73a4396245f63c7a6
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-06-26 12:39:24 +0300

    Created Authentication Function

--- backend/routers/middlewares/authentication.js
+++ backend/routers/middlewares/authentication.js
@@ -1,2 +1,24 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const secret = process.env.SECRET;
+
+const authentication = (req, res, next) => {
+  console.log("req.headers.authorization " + req.header("Authorization"));
+  if (!req.header("Authorization")) {
+    res.status(403).json({ message: "forbidden" });
+  }
+  const token = req.header("Authorization").split(" ")[1];
+  console.log("TOKEN : ", token);
+  jwt.verify(token, secret, (err, result) => {
+    if (err) {
+      return res.status(403).json({ message: "forbidden" });
+    }
+    if (result) {
+      req.token = result;
+      next();
+    }
+  });
+};
+
+module.exports = authentication;
