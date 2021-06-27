21edcf02c0d07791477b3d66459dc6fd7c00b93b
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-06-27 10:36:13 +0300

    create update activities function (controllers)

--- backend/routers/controllers/activity.js
+++ backend/routers/controllers/activity.js
@@ -16,7 +16,7 @@ const getAllActivities = (req, res) => {
     const id = req.params.id;
     const query = 'SELECT * FROM  activities INNER JOIN  users ON  users.currently_in=activities.location AND users.id=?';
     data = [id]
-    db.query(query,data, (err, result) => {
+    db.query(query, data, (err, result) => {
         if (err) throw err;
         console.log('RESULT: ', result);
         res.json(result)
@@ -27,25 +27,34 @@ const getActivitiesById = (req, res) => {
     const id = req.params.id;
     const query = 'SELECT * FROM  activities INNER JOIN  users ON activities.id=? WHERE users.id=user_id';
     data = [id]
-    db.query(query,data, (err, result) => {
+    db.query(query, data, (err, result) => {
         if (err) throw err;
         console.log('RESULT: ', result);
         res.json(result)
     });
 };
 
-const deleteActivitiesById=(req,res)=>{
+const deleteActivitiesById = (req, res) => {
     const id = req.params.id;
     const query = 'DELETE FROM  activities WHERE id=?';
     data = [id]
-    db.query(query,data, (err, result) => {
+    db.query(query, data, (err, result) => {
         if (err) throw err;
         console.log('RESULT: ', result);
         res.json(result)
     });
 };
 
-
+const updateActivitiesById = (req, res) => {
+    const id = req.params.id;
+    const query = 'UPDATE activities SET title=?, start_date=?, finish_date=? ,location=?, details=?, requirements=? , activities=? ,  images=? , estimated_budget=?   WHERE  id=?';
+    data = [title, start_date, finish_date, location, details, requirements, activities, images, estimated_budget, id];
+    db.query(query, data, (err, result) => {
+        if (err) throw err;
+        console.log('RESULT: ', result);
+        res.json(result)
+    });
+}
 
 
 
@@ -54,5 +63,6 @@ module.exports = {
     addActivity,
     getAllActivities,
     getActivitiesById,
-    deleteActivitiesById
+    deleteActivitiesById,
+    updateActivitiesById
 };
