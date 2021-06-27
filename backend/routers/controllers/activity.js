bfb99d7355ebe2e757453f61cb89bef22d3839fe
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-06-27 09:48:01 +0300

    edit on function addActivities

--- backend/routers/controllers/activity.js
+++ backend/routers/controllers/activity.js
@@ -3,7 +3,7 @@ const db = require("../../db/db")
 
 const addActivity = (req, res) => {
     const id = req.params.id;
-    const query = `INSERT INTO activities (title, start_date, finish_date ,location, details, requirements , activities ,  images , estimated_budget) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?) WHERE  id=? `;
+    const query = `INSERT INTO activities (title, start_date, finish_date ,location, details, requirements , activities ,  images , estimated_budget) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)  `;
     const { title, start_date, finish_date, location, details, requirements, activities, images, estimated_budget } = req.body
     const data = [title, start_date, finish_date, location, details, requirements, activities, images, estimated_budget, id];
     db.query(query, data, (err, results) => {
