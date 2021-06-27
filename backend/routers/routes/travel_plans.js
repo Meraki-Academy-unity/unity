7942b3669acca33a08fb3fb07ac75b406bb76cd1
deyaa1997 <deyah.mosa@gmail.com>
2021-06-27 12:03:14 +0300

    create router and check the functionality

--- backend/routers/routes/travel_plans.js
+++ backend/routers/routes/travel_plans.js
@@ -1,10 +1,17 @@
 const express = require("express");
 
-const {createTravelPlans , getAllTravelPlans , getTravelPlansById , deleteTravelPlansById} = require("../controllers/travel_plans");
+const {
+  createTravelPlans,
+  getAllTravelPlans,
+  getTravelPlansById,
+  updateTravelPlansById,
+  deleteTravelPlansById,
+} = require("../controllers/travel_plans");
 const travelPlansRouter = express.Router();
 // we will use "/:id"  instead of "/" for development stage until the auth is ready to use , so we can retrieve users id from token instead of params
 travelPlansRouter.post("/:user_id", createTravelPlans);
-travelPlansRouter.get("/" , getAllTravelPlans )
-travelPlansRouter.get("/:id" , getTravelPlansById )
-travelPlansRouter.delete("/:id" , deleteTravelPlansById)
+travelPlansRouter.get("/", getAllTravelPlans);
+travelPlansRouter.get("/:id", getTravelPlansById);
+travelPlansRouter.put("/:id", updateTravelPlansById);
+travelPlansRouter.delete("/:id", deleteTravelPlansById);
 module.exports = travelPlansRouter;
