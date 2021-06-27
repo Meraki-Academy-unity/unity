71f949d944e78c06b48d1bae031e90424c9e6dbe
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-06-27 22:32:36 +0300

    Created Join Activity Feature

--- backend/routers/controllers/activity.js
+++ backend/routers/controllers/activity.js
@@ -1,12 +1,29 @@
 const db = require("../../db/db");
 
+/*
+    title VARCHAR(255)  NOT NULL,
+    start_date DATE NOT NULL,
+    finish_date DATE NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    details VARCHAR(255), 
+    requirements VARCHAR(255) ,
+    activities VARCHAR(255) ,
+    images VARCHAR(255) ,
+    estimated_budget INT,
+    status_id INT ,
+    user_id INT,
+    creation_time  DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id ) REFERENCES users(id ),
+    FOREIGN KEY (status_id) REFERENCES status(id),
+*/
+
 const addActivity = (req, res) => {
   const id = req.params.id;
   const query_ = `SELECT currently_in FROM  users WHERE id=${id}`;
   db.query(query_, (err, results) => {
     if (err) throw err;
     const currentlyIn = results[0].currently_in;
-    const query = `INSERT INTO activities (title, start_date, finish_date ,location, details, requirements , activities ,  images , estimated_budget) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)  `;
+    const query = `INSERT INTO activities (title, start_date, finish_date ,location, details, requirements , activities ,  images , estimated_budget,user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?)  `;
     const {
       title,
       start_date,
@@ -17,6 +34,7 @@ const addActivity = (req, res) => {
       images,
       estimated_budget,
     } = req.body;
+
     const data = [
       title,
       start_date,
@@ -105,10 +123,23 @@ const updateActivitiesById = (req, res) => {
   });
 };
 
+const joinActivityById = (req, res) => {
+  const activity_id = req.params.id;
+  const user_id = req.body.user_id;
+  const query = `INSERT INTO activity_members (user_id,activity_id) VALUES (?,?)`;
+  const data = [user_id, activity_id];
+
+  db.query(query, data, (err, result) => {
+    if (result) res.status(200).json("Activity Joined Successfully !");
+    else res.status(400).json("ERROR OCCURRED.. !");
+  });
+};
+
 module.exports = {
   addActivity,
   getAllActivities,
   getActivitiesById,
   deleteActivitiesById,
   updateActivitiesById,
+  joinActivityById,
 };
