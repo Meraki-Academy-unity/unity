a269436c0fd412d365e2c930d41417cfa80690c6
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-06-30 20:39:40 +0300

    create get images by gest function (controllers)

--- backend/routers/controllers/images.js
+++ backend/routers/controllers/images.js
@@ -11,7 +11,7 @@ const addImage = (req, res) => {
     });
 };
 
-const getImageByUserId = (req,res) => {
+const getImageByUserId = (req, res) => {
     const user_id = req.token.user_id;
     const query = "SELECT images FROM images INNER JOIN users ON users.id=images.user_id AND images.user_id=?"
     const data = [user_id];
@@ -21,7 +21,20 @@ const getImageByUserId = (req,res) => {
     });
 }
 
+const getImageByGestId = (req, res) => {
+    const id = req.params.id;
+    const query = "SELECT images FROM images INNER JOIN users ON users.id=images.user_id AND images.user_id=?"
+    const data = [id];
+    db.query(query, data, (err, result) => {
+        if (result) res.status(200).json(result);
+        else res.status(400).json("ERROR !");
+    });
+
+}
+
+
 module.exports = {
     addImage,
-    getImageByUserId
+    getImageByUserId,
+    getImageByGestId
 }
