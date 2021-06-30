6b4f422dae28e0f42b34556da9f58e77d1dfada3
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-06-30 19:43:41 +0300

    add image path (route)

---
+++ backend/routers/routes/images.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const authentication = require('../middlewares/authentication')
+
+
+const ImagesRouter = express.Router();
+
+const { addImage } = require("../controllers/images");
+
+
+ImagesRouter.post("/", authentication, addImage)
