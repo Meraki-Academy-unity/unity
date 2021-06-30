7abed846b66cba7b7ef59f61cffe207443b89057
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-06-30 20:26:13 +0300

    create get images function (controllers)

--- backend/routers/controllers/images.js
+++ backend/routers/controllers/images.js
@@ -1,7 +1,7 @@
 const db = require("../../db/db");
 
 const addImage = (req, res) => {
-    const {images}=req.body;
+    const { images } = req.body;
     const user_id = req.token.user_id;
     const query = `INSERT INTO images (user_id,images) VALUES (?,?)`;
     const data = [user_id, images];
@@ -11,7 +11,17 @@ const addImage = (req, res) => {
     });
 };
 
+const getImageByUserId = () => {
+    const user_id = req.token.user_id;
+    const query = "SELECT * FROM images INNER JOIN users ON user_id=images.id WHERE user_id=?"
+    const data = [user_id];
+    db.query(query, data, (err, result) => {
+        if (result) res.status(200).json(result);
+        else res.status(400).json("ERROR !");
+    });
+}
 
 module.exports = {
-    addImage
+    addImage,
+    getImageByUserId
 }
