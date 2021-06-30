e45d9e3e136823153f4e2cbaab6b6881829ee328
deyaa1997 <deyah.mosa@gmail.com>
2021-06-30 12:23:10 +0300

    modify travel plans and get travel plane by id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import { Route } from "react-router-dom";
 import Login from "./components/auth/login/index";
 import SignUp from "./components/auth/signUp/index";
 import AddActivities from "./components/Activities/addActivities";
-import AddTravelPlans from "./components/travel_Plans";
+import AddTravelPlans from "./components/travel_Plans/index";
 import AddPerferences from "./components/preferences/addPreferences";
 import Activities from "./components/Activities/getActivities";
 import Navigation from "./components/navigation";
 import GetAllTravel from "./components/travel_Plans/getAllTravel";
 import ContactUs from './components/contactUs'
+import GetTravelById from "./components/travel_Plans/getTravelById"
 
 const App = () => {
   return (
@@ -17,13 +18,14 @@ const App = () => {
       <Route exact path="/login" render={() => <Login />} />
 	  <Route exact path="/register" render={() => <SignUp />} />
     <Route exact path="/contactUs" render={()=><ContactUs/>}/>
+    <Route exact path="/plans" component={GetAllTravel}/>
+    <Route exact path="/travelPlans/:id" component={GetTravelById} />
       {/* <Login /> */}
       {/* <SignUp /> */}
       {/* <AddActivities/> */}
       {/* <AddTravelPlans /> */}
       {/* <AddPerferences /> */}
       {/* <Activities /> */}
-      {/* <GetAllTravel /> */}
     </div>
   );
 };
