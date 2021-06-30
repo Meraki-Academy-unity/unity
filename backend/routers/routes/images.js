12e647c62f68f81f50e60a488f29de0e1120ff24
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-06-30 20:42:16 +0300

    add image path (route)

--- backend/routers/routes/images.js
+++ backend/routers/routes/images.js
@@ -4,10 +4,11 @@ const authentication = require('../middlewares/authentication')
 
 const ImagesRouter = express.Router();
 
-const { addImage,getImageByUserId } = require("../controllers/images");
+const { addImage,getImageByUserId,getImageByGestId } = require("../controllers/images");
 
 
 ImagesRouter.post("/", authentication,addImage)
-ImagesRouter.get("/", authentication,getImageByUserId)
+ImagesRouter.get("/", authentication,getImageByUserId);
+ImagesRouter.get("/:id",getImageByGestId);
 
 module.exports = ImagesRouter;
