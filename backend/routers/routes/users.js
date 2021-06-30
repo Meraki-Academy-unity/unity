fed436633793a15b0000ae9dfe92139148dcbe81
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-06-30 13:15:36 +0300

    Created Profile APIs

---
+++ backend/routers/routes/users.js
@@ -0,0 +1,11 @@
+const express =require('express');
+
+const {getUserById,getMyProfile} = require('../controllers/users.js')
+
+const profileRouter = express.Router();
+
+profileRouter.get('/:id',getUserById)
+profileRouter.get('/myProfile',getMyProfile)
+
+
+module.exports = profileRouter;
