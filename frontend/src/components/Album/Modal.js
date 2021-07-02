c31419bff2d05e0255409dbb75f41562590aa22d
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-07-02 20:03:39 +0300

    some modify on photo album comp

--- frontend/src/components/Album/Modal.js
+++ frontend/src/components/Album/Modal.js
@@ -11,17 +11,18 @@ const Modal = () => {
             photo: state.photo.photo
         };
     });
-    console.log("photoMODAL", state.photo)
-
+    
     const imageClose = (e) => {
+        if(e.target.classList.contains("backdrop")){
         dispatch(setPhoto(""))
+        }
     }
 
 
     return (
         <>
-            <div className="backdrop">
-                <img src={state.photo.images} alt="enlarged pic" onClick={imageClose} />
+            <div className="backdrop"  onClick={imageClose}>
+                <img src={state.photo.images} alt="enlarged pic" />
             </div>
         </>
     )
