a338e078d6c8d558fbe9d6d61dde32e8c68ab79b
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-07-02 17:40:23 +0300

    added some design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
       {/* <AddPerferences /> */}
       {/* <Activities /> */}
       {/* <GetAllTravel /> */}
-      <CountryList/>
+      {/* <CountryList/> */}
       <Footer/>
 
 
