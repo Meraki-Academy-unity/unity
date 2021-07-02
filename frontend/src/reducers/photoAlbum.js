e1916658767c1c166a5ed4b9bf496fe05f5df523
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-07-02 15:04:51 +0300

    create photo reducer

---
+++ frontend/src/reducers/photoAlbum.js
@@ -0,0 +1,22 @@
+
+const initialState = {
+    photo: [],
+};
+
+const photo = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case "SET_PHOTO":
+            return { photo: [...payload] };
+        default:
+            return state;
+    }
+};
+
+export default photo;
+
+export const setPhoto = (photo) => {
+    return {
+        type: "SET_PHOTO",
+        payload: photo,
+    };
+};
