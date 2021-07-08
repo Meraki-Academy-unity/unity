fadc343dc76197892ece51b3038049bcb9824e28
deyaa1997 <deyah.mosa@gmail.com>
2021-07-08 20:41:14 +0300

    User Can edit his profile

--- backend/routers/controllers/travel_plans.js
+++ backend/routers/controllers/travel_plans.js
@@ -35,7 +35,7 @@ const createTravelPlans = (req, res) => {
 };
 
 const getAllTravelPlans = (req, res) => {
-  const query = `SELECT travel_plans.title , travel_plans.activities ,travel_plans.id , travel_plans.countries ,travel_plans.countries , travel_plans.creation_time , users.first_name , users.last_name  FROM travel_plans INNER JOIN  users ON travel_plans.user_id = users.id `;
+  const query = `SELECT travel_plans.title , travel_plans.activities ,travel_plans.id , travel_plans.countries ,travel_plans.start_date , travel_plans.finish_date , travel_plans.estimated_budget , travel_plans.creation_time , users.first_name , users.last_name , users.profile_image  FROM travel_plans INNER JOIN  users ON travel_plans.user_id = users.id `;
   db.query(query, (err, results) => {
     if (err) throw err;
     res.status(200);
--- backend/routers/routes/users.js
+++ backend/routers/routes/users.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authentication = require('../middlewares/authentication')
-const { getUserById, getMyProfile } = require("../controllers/users.js");
+const { getUserById, getMyProfile , updateMyProfile } = require("../controllers/users.js");
 
 const profileRouter = express.Router();
 
 profileRouter.get("/user/:id", getUserById);
 profileRouter.get("/myProfile",authentication, getMyProfile);
+profileRouter.put("/myProfile",authentication, updateMyProfile)
+
 
 module.exports = profileRouter;
