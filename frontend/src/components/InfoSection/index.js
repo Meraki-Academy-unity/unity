0ee514475605df500672b1b328e0236c7e100999
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-07-09 21:43:44 +0300

    Finished Components on infoSection

--- frontend/src/components/InfoSection/index.js
+++ frontend/src/components/InfoSection/index.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import {
+    InfoContainer,
+    InfoWrapper,
+    InfoRow,
+    Column1,
+    TextWrapper,
+    TopLine,
+    Heading,
+    SubTitle,
+    BtnWrap
+} from './infoStyle'
+import {Button} from '../HeroSection/ButtonStyle'
 
 function InfoSection() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <>
+      <InfoContainer id={id}>
+        <InfoWrapper>
+          <InfoRow>
+            <Column1>
+              <TextWrapper>
+                <TopLine>TopLine</TopLine>
+                <Heading>Heading</Heading>
+                <SubTitle>SubTitle</SubTitle>
+                <BtnWrap>
+                  <Button to="/">Home</Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  );
 }
 
-export default InfoSection
+export default InfoSection;
