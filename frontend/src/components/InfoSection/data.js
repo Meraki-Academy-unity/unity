224608d3d6342feac387d062761dde4520550aed
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-07-10 20:46:43 +0300

    Added Data folder forinfo

---
+++ frontend/src/components/InfoSection/data.js
@@ -0,0 +1,16 @@
+export const HomeOnjOne = {
+    id:'about',
+    lightBg : false,
+    lightTex:true,
+    lightTextDesc:true,
+    topLine:'Premium Bank',
+    headLine : 'Unlimited Transaction with zero fees',
+    description:'Get access to our exclusive app that allows you to send unlimited trasactions witjout geting paid',
+    buttonLabel:'Get Started',
+    imgStart:false,
+    img:require('../../Images/group.png'),
+    alt:'group',
+    dark:true,
+    primary:true,
+    darkText:false
+}
