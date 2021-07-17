da790f98c68083cfe224bb3a342a70ae035951ce
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-07-17 12:14:51 +0300

    css on user show preferences

--- frontend/src/components/preferences/showUserPreference.js
+++ frontend/src/components/preferences/showUserPreference.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import moment from 'moment';
-import { Link, Route, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 
 const GetUserPreferences = () => {
@@ -28,16 +28,20 @@ const GetUserPreferences = () => {
 
     return (
         <>
-            {preferences && preferences.map((elem, i) => {
-                return (<div key={i}>
-                    <p> loaction: {elem.location}</p>
-                    <p>Date : from {moment(elem.start_date, "YYYY-MM-DD").format("DD-MM-YYYY")} To {moment(elem.finish_date, "YYYY-MM-DD").format("DD-MM-YYYY")}</p>
-                    <p>Activities: {elem.activities}</p>
-                    <p>Same Gender: {elem.same_gender ? "Yes" : "No"}</p>
-                    <p>Same Age: {elem.similar_age ? "Yes" : "No"}</p>
-                </div>)
-            })
-            }
+            {preferences.length == 0 ? <div><p> You dont have preferences please fill your preferences </p></div> :
+                <div className="pref_page">
+                    {preferences && preferences.map((elem, i) => {
+                        return (<div key={i}>
+                            <p>  Desired Travel Destinations :  {elem.location}</p>
+                            <p> Availability Date : from {moment(elem.start_date, "YYYY-MM-DD").format("DD-MM-YYYY")} &nbsp;to &nbsp;
+                                {moment(elem.finish_date, "YYYY-MM-DD").format("DD-MM-YYYY")}</p>
+                            <p>Activities : {elem.activities}</p>
+                            <p>Same Gender : {elem.same_gender ? "Yes" : "No"}</p>
+                            <p>Same Age : {elem.similar_age ? "Yes" : "No"}</p>
+                        </div>)
+                    })
+                    }
+                </div>}
         </>
     )
 };
